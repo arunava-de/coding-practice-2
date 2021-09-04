3c24a83068edc5b10d723ad227cfae5dce732b68
Arunava De <dearunava123@gmail.com>
2021-09-04 23:54:46 +0530

    Adding for 4th September

--- count_smaller_opt.py
+++ count_smaller_opt.py
@@ -1,28 +1,58 @@
-from math import ceil, log2
+def count_smaller(nums):
 
-class SegTree:
-    def __init__(self, arr):
-        
-        n = len(arr)
-        x = int(ceil(log2(n)))
-        size = 2*(2**x) - 1
+    counts = [0]*len(nums)
+    idx_map = [(nums[i], i) for i in range(len(nums))]
 
-        self.ST = [None]*size 
-        self.construct_seg_tree(arr, 0, n-1, 0)
+    merge_sort_counts(idx_map, counts, 0, len(nums))
 
-    def construct_seg_tree(self, arr, s, e, si):
+    return counts 
 
-        if si<s or si>e:
-            return 0 
-        
-        if s==e:
-            self.ST[si] = arr[s]
-            return arr[s]
+def merge_sort_counts(idx_map, counts, l, r):
 
-        mid = (s+e)//2 
+    if r-l<=1:
+        return
 
-        self.ST[si] = self.construct_seg_tree(arr, s, mid, 2*si + 1) + self.construct_seg_tree(arr, mid+1, e, 2*si + 2)
+    mid = (l+r)//2
 
-        return self.ST[si]
+    merge_sort_counts(idx_map, counts, l, mid)
+    merge_sort_counts(idx_map, counts, mid, r)
+
+    return merge(idx_map, l, mid, r, counts)
+
+def merge(idx_map, l, mid, r, counts):
+
+    combo = []
+
+    i = l
+    j = mid
+
+    while i<mid and j<r:
+        if idx_map[i][0]<=idx_map[j][0]:
+            counts[idx_map[i][1]] += j-mid
+            combo.append(idx_map[i])
+            i += 1
+        else:
+            combo.append(idx_map[j])
+            j += 1
+    
+    while i<mid:
+        counts[idx_map[i][1]] += j-mid
+        combo.append(idx_map[i])
+        i += 1
+
+    while j<r:
+        combo.append(idx_map[j])
+        j += 1
+
+    for i in range(l,r):
+        idx_map[i] = combo[i-l]
+
+nums = [7,2,5,4,1,6]
+count_smaller(nums)
+
+nums = [5,2,6,1]
+count_smaller(nums)
+
+
 
     
